799cb90269807c1fea5b1b084d874c534a84553b
asraful7711 <asraful7711@gmail.com>
2022-11-13 01:02:04 +0600

    successfully show overal time in record component

--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Author from '../Author/Author';
-const Records = (props) => {
-    // console.log(props.time)
+const Records = ({ record }) => {
+    let totalTime = 0;
+    for (const detail of record) {
+        totalTime = totalTime + detail.time
+    }
     return (
         <div className='bg-white px-7 sticky top-0'>
             <Author></Author>
@@ -11,9 +14,14 @@ const Records = (props) => {
                 <button className=' btn btn-circle text-lg bg-blue-300 border-none'>30s</button>
                 <button className=' btn btn-circle text-lg bg-blue-300 border-none'>40s</button>
             </div>
-            <div className="">
-                <p>Exercise time <span>{props.record.length}seconds</span></p>
-                <p>Break time <span>200 seconds</span></p>
+            <div className="my-6 text-lg font-bold">
+                <p className='mb-3'>Exercise Details</p>
+                <p className='py-3 px-2 flex justify-between bg-slate-200 mb-4 rounded-md'>
+                    <span>Exercise time:</span> <span className='text-gray-400'> {totalTime} sec</span>
+                </p>
+                <p className='py-3 px-2 flex justify-between bg-slate-200 rounded-md'>
+                    <span>Exercise time:</span> <span className='text-gray-400'>sec</span>
+                </p>
             </div>
         </div>
     );
